764faac542a3f6d0e2fd3912462dcd9983f00834
noturCR <noturcr@gmail.com>
2025-04-22 15:07:53 +0530

    Create lcs.cpp

---
+++ lcs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lcs(string X, string Y) {
+    int m = X.length(), n = Y.length();
+    vector<vector<int>> L(m+1, vector<int>(n+1, 0));
+    for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= n; j++)
+            L[i][j] = (X[i-1] == Y[j-1]) ? 1 + L[i-1][j-1] : max(L[i-1][j], L[i][j-1]);
+    return L[m][n];
+}
+
+int main() {
+    string X = "AGGTAB", Y = "GXTXAYB";
+    cout << "Length of LCS is " << lcs(X, Y);
+    return 0;
+}
